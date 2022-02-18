ce192121239231cf7c1fc98333d37bf74b8147c0
Lyra <git@lyra.bz>
2022-02-18 17:22:50 +0100

    added chat log

---
+++ ChatApp/ChatApp/Pages/Chat.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ChatApp.Pages
+{
+    public class ChatViewModel : ComponentBase
+    {
+        protected List<string> messages = new();
+
+        protected string currentMessage;
+
+        protected override void OnInitialized()
+        {
+            this.messages.Add("Test");
+            this.messages.Add("Test123");
+        }
+
+        protected void SendMessage()
+        {
+            this.messages.Add(this.currentMessage);
+            this.currentMessage = String.Empty;
+        }
+    }
+}
