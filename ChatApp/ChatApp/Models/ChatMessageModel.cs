41047192451de47d8f9a26d84804448eb16334a3
Lyra <git@lyra.bz>
2022-02-18 17:27:18 +0100

    added user to messages

---
+++ ChatApp/ChatApp/Models/ChatMessageModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatApp.Models
+{
+    public class ChatMessageModel
+    {
+        public string Username { get; set; }
+        public string Message { get; set; }
+    }
+}
--- ChatApp/ChatApp/Pages/Chat.razor.cs
+++ ChatApp/ChatApp/Pages/Chat.razor.cs
@@ -1,22 +1,37 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using ChatApp.Models;
+using Microsoft.AspNetCore.Components;
 
 namespace ChatApp.Pages
 {
     public class ChatViewModel : ComponentBase
     {
-        protected List<string> messages = new();
+        protected List<ChatMessageModel> messages = new();
 
         protected string currentMessage;
 
         protected override void OnInitialized()
         {
-            this.messages.Add("Test");
-            this.messages.Add("Test123");
+            this.messages.Add(new ChatMessageModel()
+            {
+                Username = "Thomas",
+                Message = "Hallo"
+            });
+
+            this.messages.Add(new ChatMessageModel()
+            {
+                Username = "Thomas",
+                Message = "Grüß Gott"
+            });
         }
 
         protected void SendMessage()
         {
-            this.messages.Add(this.currentMessage);
+            this.messages.Add(new ChatMessageModel()
+            {
+                Username = "Thomas",
+                Message = this.currentMessage
+            });
+
             this.currentMessage = String.Empty;
         }
     }
