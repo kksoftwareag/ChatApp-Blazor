c62d6c704cd4ecb350d8a63bc3f2fd4a0c5ea84b
Lyra <git@lyra.bz>
2022-05-06 17:40:59 +0200

    added js interop to save username and scroll chatview to bottom

--- ChatApp/ChatApp/Pages/Chat.razor.cs
+++ ChatApp/ChatApp/Pages/Chat.razor.cs
@@ -2,24 +2,60 @@
 using ChatApp.Models;
 using ChatApp.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using Microsoft.Toolkit.Mvvm.Messaging;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace ChatApp.Pages
 {
-    public class ChatViewModel : ComponentBase, IRecipient<ChatMessageSentMessage>
+    public class ChatViewModel : ComponentBase, IRecipient<ChatMessageSentMessage>, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         [Inject]
         public ChatMessageService ChatMessageService { get; set; }
 
         [Inject]
         public IMessenger Messenger { get; set; }
 
-        protected string currentUser;
+        [Inject]
+        public IJSRuntime JsRuntime { get; set; }
+
+        private string currentUsername;
+        public string CurrentUsername
+        {
+            get => this.currentUsername;
+            set
+            {
+                this.currentUsername = value;
+                this.RaisePropertyChanged();
+            }
+        }
 
         protected string currentMessage;
 
-        protected override void OnInitialized()
+        public ChatViewModel()
         {
+            this.PropertyChanged += ChatViewModel_PropertyChanged;
+        }
+
+        private void ChatViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(this.CurrentUsername))
+            {
+                _ = this.SaveUsername();
+            }
+        }
+
+        private async Task SaveUsername()
+        {
+            await this.JsRuntime.InvokeVoidAsync("window.ChatApp.SetUsername", this.CurrentUsername);
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            this.CurrentUsername = await this.JsRuntime.InvokeAsync<string>("window.ChatApp.GetUsername");
             this.Messenger.Register<ChatMessageSentMessage>(this);
         }
 
@@ -27,7 +63,7 @@ namespace ChatApp.Pages
         {
             this.ChatMessageService.SendMessage(new ChatMessageModel()
             {
-                Username = this.currentUser,
+                Username = this.CurrentUsername,
                 Message = this.currentMessage
             });
 
@@ -36,7 +72,16 @@ namespace ChatApp.Pages
 
         public void Receive(ChatMessageSentMessage message)
         {
-            this.InvokeAsync(() => this.StateHasChanged());
+            this.InvokeAsync(async () =>
+            {
+                this.StateHasChanged();
+                await this.JsRuntime.InvokeVoidAsync("window.ChatApp.ScrollToBottom");
+            });
+        }
+
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
---
+++ ChatApp/ChatApp/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿window.ChatApp = {
+    SetUsername: function (username) {
+        localStorage.setItem('username', username);
+    },
+    GetUsername: function () {
+        return localStorage.getItem('username');
+    },
+    ScrollToBottom: function () {
+        let chatlog = document.querySelector('.chatlog');
+        let lastChild = chatlog.lastChild;
+
+        if (lastChild) {
+            lastChild.scrollIntoView();
+        }
+    }
+};
